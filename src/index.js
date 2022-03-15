6d4a4ac8fd3af1eea239e1a2f1c9f3a65ce093f7
VelOuria <sofleos@gmail.com>
2022-03-15 11:04:50 -0600

    deleted unnecesary comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-//import "./index.css"
 import GlobalStyles from "./styles"
-//import App from "./App"
 import MainForm from "./MainForm"
-//import reportWebVitals from "./reportWebVitals"
 
 ReactDOM.render(
   <React.StrictMode>
